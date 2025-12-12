2b1b44966f3cbc1a60190c42356948ebdd8a9d19
Kay <amerk86@gmail.com>
2025-12-12 13:07:12 +1000

    feat: Installed shadcn-vue and tailwindcss latest

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="container max-w-6xl mx-auto py-16">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+
+        <UiButton class="mt-6">Click me</UiButton>
+    </section>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    // prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+
+  css: ['./app/assets/css/main.css'],
+  vite: { plugins: [tailwindcss(),], },
+  modules: ['shadcn-nuxt'],
 })
